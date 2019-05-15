4d2c648ac9a6da774d6d797541ea3bca17d885be
John Begenisich <john.begenisich@outlook.com>
2019-05-15 11:42:22 +0800

    check all mounted partitions; require psutil

--- low-disk-check.py
+++ low-disk-check.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 
-import os
 import argparse
+import psutil
 
 
 def sizeof_fmt(num, suffix='B'):
@@ -13,60 +13,32 @@ def sizeof_fmt(num, suffix='B'):
 
 
 def filesystem_used_header():
-    return '%10s %10s %10s %6s' % ('Filesystem', 'Size', 'Used', 'Used%')
-
-
-def filesystem_used_string(f):
-    return '%-10s %10s %10s %5d%%' % (
-        f['filesystem'],
-        sizeof_fmt(f['size']),
-        sizeof_fmt(f['used']),
-        f['percent_used'])
+    return
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Alert if disk space is low')
-    parser.add_argument('path', nargs='*', default=[])
+    parser = argparse.ArgumentParser(description='Print an alert if disk space is low')
     parser.add_argument('--used', type=int, default=90, metavar='percent')
-    parser.add_argument('-v', action='store_true')
     args = parser.parse_args()
 
-    if args.path:
-        paths_to_check = args.path
-    else:
-        paths_to_check = ['/', '/boot', '/home']
-
-    if args.v:
-        print('Checking', ' '.join(paths_to_check))
-
+    partitions = psutil.disk_partitions()
     filesystems_toobig = []
-    if args.v:
-        print(filesystem_used_header())
-    for path in paths_to_check:
-        statvfs = os.statvfs(path)
-        size = statvfs.f_frsize * statvfs.f_blocks   # size of filesystem
-        free = statvfs.f_frsize * statvfs.f_bfree    # actual free bytes
-        avail = statvfs.f_frsize * statvfs.f_bavail  # available to users
-        used = size - avail
-        percent_used = float(used) / float(size) * 100.0  # Python 2, sigh
-        f = {'filesystem': path,
-             'used': used,
-             'size': size,
-             'percent_used': int(percent_used)}
-        if args.v:
-            print(filesystem_used_string(f))
-        if percent_used >= args.used:
-            filesystems_toobig.append(f)
+    longest_path = 0
+    for part in partitions:
+        path = part.mountpoint
+        usage = psutil.disk_usage(path)
+        if usage.percent >= args.used:
+            filesystems_toobig.append((path, sizeof_fmt(usage.total), sizeof_fmt(usage.used), usage.percent))
+            if len(path) > longest_path: longest_path = len(path)
 
     if not filesystems_toobig:
         return
 
-    if args.v:
-        print()
+    longest_path = max(len('Filesystem'), longest_path)
     print('Alert: the following filesystems are >= %d%% full:\n' % args.used)
-    print(filesystem_used_header())
-    for f in filesystems_toobig:
-        print(filesystem_used_string(f))
+    print('%-*s %10s %10s %6s' % (longest_path, 'Filesystem', 'Size', 'Used', 'Used%'))
+    for path, size, used, percent in filesystems_toobig:
+        print('%-*s %10s %10s %5d%%' % (longest_path, path, size, used, percent))
 
 
 # -------------------------------------------------
